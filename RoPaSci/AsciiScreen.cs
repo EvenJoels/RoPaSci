a5a3bd3bacdbc15e5af5ccbe7eb0f5f3e60dfba5
EvenJoels <Joelevans1305@gmail.com>
2025-07-26 15:07:46 +0100

    Added main classes and some UI

---
+++ RoPaSci/AsciiScreen.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoPaSci
+{
+    internal class AsciiScreen
+    {
+        public static void SetUpConsole()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.Title = "RoPaSci - A Rock Paper Scissors Game";
+            Console.CursorVisible = false;
+
+            // Could add settings like window size, but remember to use Try catch
+        }
+
+        public static void ShowWelcomeScreen()
+        {
+            Console.Clear();
+            WriteCentredText("Welcome to RoPaSci!", 2);
+            WriteCentredText("A Rock, Paper, Scissors Game", 3);
+            WriteCentredText("Press any key to start...", 5);
+            Console.ReadKey(true);
+        }
+
+
+
+        private static void WriteCentredText(string text, int row)
+        {
+            int leftPad = (Console.WindowWidth - text.Length) / 2;
+            Console.SetCursorPosition(leftPad, row);
+            Console.Write(text);
+        }
+    }
+}
--- RoPaSci/Program.cs
+++ RoPaSci/Program.cs
@@ -1,9 +1,21 @@
-﻿using System;
+﻿using RoPaSci;
+using System;
 
 public class Program
 {
     public static void Main(string[] args)
     {
+        // Set up console settings
+        AsciiScreen.SetUpConsole();
+
+        // Ask game mode and AI setting
+        AsciiScreen.ShowWelcomeScreen();
+
+        // Game loop
+
+
+        Console.WriteLine("\nPress any key to exit...");
+        Console.ReadKey(true);
 
     }
 }
